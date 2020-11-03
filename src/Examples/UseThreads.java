d677f68dcac93a441ed0f1bc9b9628775ff57745
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-11-03 19:37:20 +0530

    add code for the run() method

--- src/Examples/UseThreads.java
+++ src/Examples/UseThreads.java
@@ -9,7 +9,16 @@ class MyThread implements Runnable {
 
     @Override
     public void run() {
-
+        System.out.println(thrdName + "starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + thrdName + ", count is " + count);
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(thrdName + "interrupted.");
+        }
+        System.out.println(thrdName + " terminating.");
     }
 }
 
