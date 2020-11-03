106ae27693cd3992fec408c98503d5e1697a16b7
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-11-03 19:25:39 +0530

    add constructor for class MyThread

--- src/Examples/UseThreads.java
+++ src/Examples/UseThreads.java
@@ -1,6 +1,12 @@
 package Examples;
 
 class MyThread implements Runnable {
+    String thrdName;
+
+    MyThread(String name) {
+        thrdName = name;
+    }
+
     @Override
     public void run() {
 
